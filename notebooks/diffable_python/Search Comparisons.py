540f79b9fbcd84adefafa9d0c83f68617ff5af37
NickCEBM <nicholas.devito@phc.ox.ac.uk>
2023-02-26 01:46:24 +0000

    Finalising inter-rater reliability analysis
    Wrapping up the analysis and making sure we can get the Cohen's Kappa from SciKit up and running.

--- notebooks/diffable_python/Search Comparisons.py
+++ notebooks/diffable_python/Search Comparisons.py
@@ -52,6 +52,9 @@ df = reco_data[reco_data.euctr_res_nd.notnull()].merge(study_data[['euctr_id', '
 len(df)
 
 # + trusted=true
+df.columns
+
+# + trusted=true
 #Did we extract the same information about EUCTR results?
 
 # + trusted=true
@@ -63,6 +66,11 @@ reco_checker(df, 'euctr_res_nd', 'euctr_res_2nd')
 1 - (len(reco_checker(df, 'euctr_res_nd', 'euctr_res_2nd'))/len(df))
 
 # + trusted=true
+#cohen's kappa
+
+cohen_kappa_score(df.euctr_res_nd, df.euctr_res_2nd)
+
+# + trusted=true
 #Now did we find a ClinicalTrials.gov cross registration?
 
 # + trusted=true
@@ -72,124 +80,69 @@ reco_checker(df, 'nct_nd', 'nct_2nd')[['trial_id', 'nct_nd', 'nct_2nd', 'nct_id'
 1 - (len(reco_checker(df, 'nct_nd', 'nct_2nd'))/len(df))
 
 # + trusted=true
+cohen_kappa_score(df.nct_nd, df.nct_2nd)
+
+# + trusted=true
 #When we did both find a ClinicalTrials.gov registration, did we find the same one
 
 # + trusted=true
 df[(df.nct_nd == 1) & (df.nct_2nd == 1)].nct_match.value_counts()
 
 # + trusted=true
-1 - (9/(158+2))
+158/160
 
-# +
+# + trusted=true
 #What about finding the same ISRCTN registration
 
 # + trusted=true
 reco_checker(df, 'isrctn_nd', 'isrctn_2nd')
-# +
-
+# + trusted=true
+1 - (len(reco_checker(df, 'isrctn_nd', 'isrctn_2nd'))/len(df))
 
 
 # + trusted=true
+cohen_kappa_score(df.isrctn_nd, df.isrctn_2nd)
 
+
+# +
+#Now for publications
+
+# + trusted=true
+reco_checker(df, 'pub_nd', 'pub_2nd')
+
+# + trusted=true
+1 - (len(reco_checker(df, 'pub_nd', 'pub_2nd'))/len(df))
+
+# + trusted=true
+cohen_kappa_score(df.pub_nd, df.pub_2nd)
+
+# +
+#Did we find the same publication?
+
+# + trusted=true
+df[(df.pub_nd == 1) & (df.pub_2nd == 1)].pub_match.value_counts()
+
+# + trusted=true
+98/110
+
+# +
+#When we both found the same pub, did the extracted publication date match
+
+# + trusted=true
+df[(df.pub_match == 1)].pub_date_match.value_counts()
+
+# + trusted=true
+67/98
+
+# +
+#When we both found the same pub, did the extracted trial ID match
+
+# + trusted=true
+df[(df.pub_match == 1)].pub_reg_match.value_counts()
+
+# + trusted=true
+86/(12+86)
 # -
 
-
-# +
-
-# -
-
-
-df.to_csv(parent + '/data/dual_coding/' + 'df_temp.csv')
-
-
-
-
-
-
-
-
-
-# + trusted=true
-#First lets look at any where we didn't agree on EUCTR results.
-
-df[((df.euctr_res_nd == 1) & (df.euctr_res_2nd == 0)) | ((df.euctr_res_nd == 0) & (df.euctr_res_2nd == 1))]
-
-# + trusted=true
-1/241
-
-# + trusted=true
-1 - (7/241)
-
-# + trusted=true
-#Then anywhere we didn't both find a matched CTGov reg
-
-df[((df.nct_nd == 1) & (df.nct_2nd == 0)) | ((df.nct_nd == 0) & (df.nct_2nd == 1))]
-
-# + trusted=true
-#Now lets see whether we found the same NCT ID
-#1 means we matched
-#2 is N/A meaning there was nothing to match
-#0 is we found different things that needed to be resolved
-
-df.nct_match.value_counts()
-
-# + trusted=true
-1 - (9/(157+9))
-
-# + trusted=true
-#Then anywhere we didn't both find a matched isrctn reg
-
-df[((df.isrctn_nd == 1) & (df.isrctn_2nd == 0)) | ((df.isrctn_nd == 0) & (df.isrctn_2nd == 1))]
-
-# + trusted=true
-#How many did either of us find a publication:
-
-len(df[(df.pub_nd == 1) | (df.pub_2nd == 1)])
-
-# + trusted=true
-len(df[(df.pub_nd == 0) | (df.pub_2nd == 0)])
-
-# + trusted=true
-#Then we check publications
-
-len(df[((df.pub_nd == 1) & (df.pub_2nd == 0)) | ((df.pub_nd == 0) & (df.pub_2nd == 1))])
-
-# + trusted=true
-1 - (37/250)
-
-# + trusted=true
-#Then did we match?
-
-df.pub_match.value_counts()
-
-# + trusted=true
-98 / (98+49)
-
-# + trusted=true
-df.head(2)
-
-# + trusted=true
-#Did the publication date we extracted match?
-
-df.pub_date_match.value_counts()
-
-# + trusted=true
-1 - (80/(80+67))
-
-# + trusted=true
-80+67
-
-# + trusted=true
-df[((df.pub_reg_nd == 1) & (df.pub_reg_2nd == 0)) | ((df.pub_reg_nd == 0) & (df.pub_reg_2nd == 1))]
-
-# + trusted=true
-df.pub_reg_match.value_counts()
-
-# + trusted=true
-93+16
-
-# + trusted=true
-16 / (93+16)
-# +
 
 
